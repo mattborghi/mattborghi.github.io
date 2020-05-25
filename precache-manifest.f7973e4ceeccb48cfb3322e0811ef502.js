dcbb34ff1a87b73f8f2daf71b5d629ece45e04d7
Matias Borghi <borghi.matias@gmail.com>
2020-05-25 15:45:04 -0300

    Updates

---
+++ precache-manifest.f7973e4ceeccb48cfb3322e0811ef502.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26970b5274318622df0b62f1f5022d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d4d2d7e7f98a3256b46",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d061805febf50711f07",
+    "url": "/static/js/2.c46dd0e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c46dd0e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4d2d7e7f98a3256b46",
+    "url": "/static/js/main.e6781f15.chunk.js"
+  },
+  {
+    "revision": "ddeef44717791cca94e8",
+    "url": "/static/js/runtime-main.3d522129.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
